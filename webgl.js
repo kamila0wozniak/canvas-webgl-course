9d7736148659246d3bac94c75b0e88f5f981743b
DESKTOP-GLTSSF0\DreamMachines <kamila0wozniak@gmail.com>
2021-06-16 23:46:37 +0200

    Last day of course

--- webgl.js
+++ webgl.js
@@ -3,6 +3,7 @@ const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 const eases = require('eases');
 const BezierEasing = require('bezier-easing');
+const glslify = require('glslify');
 
 global.THREE = require('three');
 
@@ -23,7 +24,7 @@ const sketch = ({ context }) => {
     canvas: context.canvas
   });
 
-  renderer.setClearColor('hsl(0,0%, 95%)', 1.0);
+  renderer.setClearColor('hsl(0, 0%, 100%)', 1.0);
 
 
   const camera = new THREE.OrthographicCamera();
@@ -34,10 +35,38 @@ const sketch = ({ context }) => {
 
   const palette = random.pick(palettes);
 
-  for (let i=0; i<40; i++){
+  const meshes = [];
+
+  const fragmentShader = `
+    varying vec2 vUv;
+    uniform vec3 color;
+    void main (){
+      gl_FragColor=vec4(vec3(color * vUv.x), 1.0);
+    }
+  `;
+
+  const vertexShader = glslify(`
+    varying vec2 vUv;
+    uniform float time;
+    #pragma glslify: noise = require('glsl-noise/simplex/4d');
+    void main () {
+      vUv = uv;
+      vec3 pos = position.xyz * sin(time);
+      pos += noise(vec4(position.xyz, time)) * 1.0;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0); 
+     }
+  `);
+
+  for (let i = 0; i < 40; i++){
     const mesh = new THREE.Mesh(
         box,
-        new THREE.MeshStandardMaterial({
+        new THREE.ShaderMaterial({
+          fragmentShader,
+          vertexShader,
+          uniforms: {
+            color: { value: new THREE.Color(random.pick(palette)) },
+            time:{ value: 0 }
+          },
           color: random.pick(palette),
         })
     );
@@ -53,6 +82,7 @@ const sketch = ({ context }) => {
     );
     mesh.scale.multiplyScalar(0.5);
     scene.add(mesh);
+    meshes.push(mesh);
   }
 
   scene.add(new THREE.AmbientLight('hsl(0, 0%, 40%)'));
@@ -87,9 +117,12 @@ const sketch = ({ context }) => {
 
       camera.updateProjectionMatrix();
     },
-    render({ playhead }) {
+    render({ playhead, time }) {
       const t = Math.sin(playhead * Math.PI * 2);
       scene.rotation.z = eases.expoInOut(t);
+      meshes.forEach(mesh => {
+        mesh.material.uniforms.time.value = time;
+      });
       renderer.render(scene, camera);
     },
     unload() {
